2f69aef296f4f71e9bfb9bdb4340c9ddea975887
Thalubinho <thaleshenriquest@gmail.com>
2024-09-25 22:09:42 -0300

    i forgot to create the folders
    # Folders
    - transformations:
    games_formated_release_data;
    games_linux(missing);
    portuguese_supported_games(missing;
    - ordinations

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,27 @@
 package app;
 
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 	public static void main(String[] args) {
+		// creating folders "transformations" and "ordinations"
+		Path path_transformations = Paths.get("transformations");
+		Path path_ordinations = Paths.get("ordinations");
+		try {
+			Files.createDirectory(path_transformations);
+			Files.createDirectory(path_ordinations);
+		} catch (FileAlreadyExistsException e) {
+			e.printStackTrace();
+			System.err.println("O(s) diretorio(s) 'transformations' e/ou 'ordinations' ja existe");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Erro ao criar o(s) diretorio(s)");
+		}
+		
 		Transformations.transformation1();
 	}
 }
